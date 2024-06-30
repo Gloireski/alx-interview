57878addff1b858c84c25ef62856717f57964f29
Gloireski <belemgloirebekoutou8@gmail.com>
2024-06-30 10:43:38 -0700

    task done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    pascal = []
+    lp = []
+    for j in range(n):
+        nl = lp + [1]
+        for i in range(len(lp) - 1):
+            nl[i + 1] = lp[i] + lp[i + 1]
+        lp = nl
+        pascal.append(nl)
+    # print(pascal)
+    return(pascal)
